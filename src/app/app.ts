5c51b74c754d10154e4220ff1c11b9a620b5066e
Youssef Ait Rais <copybuisness007@gmail.com>
2025-07-28 16:10:28 +0200

    register page

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Register} from './components/pages/auth/register/register';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Register],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
