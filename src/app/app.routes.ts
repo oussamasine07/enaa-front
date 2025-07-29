8cd99a8418a38d94317883371bb3bab46b4c993d
oussamasine07 <sineoussama@gmail.com>
2025-07-29 12:30:20 +0100

    resolve conflict

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+<<<<<<< HEAD
 import {Register} from './components/pages/auth/register/register';
 
 export const routes: Routes = [
@@ -6,4 +7,13 @@ export const routes: Routes = [
     path: "register",
     component: Register
   }
+=======
+import { Layout } from './components/layout/layout';
+
+export const routes: Routes = [
+    {
+        path: "layout",
+        component: Layout
+    }
+>>>>>>> bb2bd34 (generate layout)
 ];
