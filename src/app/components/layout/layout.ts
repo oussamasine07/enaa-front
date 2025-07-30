8372c1e2c5c33205e9e90f598b18765d740982be
oussamasine07 <sineoussama@gmail.com>
2025-07-30 19:01:21 +0100

    make navbar components

--- src/app/components/layout/layout.ts
+++ src/app/components/layout/layout.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Sidebar } from '../partials/sidebar/sidebar';
+import { Navbar } from '../partials/navbar/navbar';
 
 @Component({
   selector: 'app-layout',
   imports: [
-    Sidebar
+    Sidebar, Navbar
   ],
   templateUrl: './layout.html',
   styleUrl: './layout.css'
