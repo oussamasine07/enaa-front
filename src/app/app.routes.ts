2a8f60fb8155c4aa54090a9c544a03c50aa44071
oussamasine07 <sineoussama@gmail.com>
2025-07-30 19:40:11 +0100

    make dashboard page and fix sidebar layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,19 @@
 import { Routes } from '@angular/router';
 import {Register} from './components/pages/auth/register/register';
 import { Layout } from './components/layout/layout';
+import { Dashboard } from './components/pages/dashboard/dashboard';
 
   
 export const routes: Routes = [
     {
-        path: "layout",
-        component: Layout
+        path: "app",
+        component: Layout,
+        children: [
+            {
+                path: 'dashboard',
+                component: Dashboard
+            }
+        ]
     },
     {
         path: "register",
--- src/app/components/layout/layout.ts
+++ src/app/components/layout/layout.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { Sidebar } from '../partials/sidebar/sidebar';
 import { Navbar } from '../partials/navbar/navbar';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
   imports: [
-    Sidebar, Navbar
+    Sidebar, Navbar, 
+    RouterOutlet
   ],
   templateUrl: './layout.html',
   styleUrl: './layout.css'
