43008e8e7ee7ac2aa9816dd21811866e7f1450cc
younes-oss <aitchattouyounes@gmail.com>
2025-08-01 18:45:42 +0200

    page rendu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import {Register} from './components/pages/auth/register/register';
 import { Layout } from './components/layout/layout';
 import { Dashboard } from './components/pages/dashboard/dashboard';
 import { BriefFormComponent } from './components/pages/brief-form/brief-form.component';
+import { Rendu } from './components/pages/rendu/rendu';
 
   
 export const routes: Routes = [
@@ -17,6 +18,10 @@ export const routes: Routes = [
             {
                 path: 'brief',
                 component: BriefFormComponent
+            },
+            {
+                path:'rendu',
+                component:Rendu
             }
         ]
     },
---
+++ src/app/components/pages/rendu/rendu.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rendu',
+  imports: [],
+  templateUrl: './rendu.html',
+  styleUrl: './rendu.css'
+})
+export class Rendu {
+
+}
